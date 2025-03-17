eb016efa6877d7aa124d3d9fea1c3b11814b0eac
bmalikay <baktybekovamalika29@gmail.com>
2025-03-18 05:44:37 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import MainPage from "./pages/mainPage/MainPage";
+import ErrorPage from "./pages/errorPage/ErrorPage";
+function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const name = prompt('Как вас зовут?');
+        const lastName = prompt('Ваша фамилия?');
+        setUser({ name, lastName });
+    }, []);
+
+    if (!user) {
+        return <div>Загрузка...</div>;
+    }
+
+    if (user.name === 'John' && user.lastName === 'Johns') {
+        return <MainPage user={user} />;
+    } else {
+        return <ErrorPage user={user} />;
+    }
+}
+
+export default App;
